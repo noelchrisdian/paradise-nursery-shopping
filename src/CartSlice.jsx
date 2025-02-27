feb3d63781db637957fa80ac84d05a6353512305
noelchrisdian <noelchrisdian@gmail.com>
2025-02-27 12:18:50 +0700

    Updated ProductList.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,14 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+
     },
     removeItem: (state, action) => {
+
     },
     updateQuantity: (state, action) => {
 
-    
+      
     },
   },
 });
